18b6ab0f9072cb23a86fe0a1a0c221eb6f075aa8
highdutch <dick.heijink@gmail.com>
2016-07-31 02:23:08 +0200

    WIP

--- Std.Core/cls/Std/ECMA/JSON.js
+++ Std.Core/cls/Std/ECMA/JSON.js
@@ -5,9 +5,10 @@
     //@param text {string} source text to parse
     //@param reviver {function?} optional reviver for parse results of child values
     //@return {*} JSON null, boolean, number, string, array or object
+    //@except when source is invalid JSON
     parse: JSON.parse,
     //@ Convert JSON value to its string representation.
-    //@param value {string} JSON value to convert
+    //@param value {*} JSON value to convert
     //@param replacer {function?} optional replacer for unparse results of child values
     //@param space {string|number?} optional space for separation
     //@return {string} string representation
